72c6ec825f0a13240492320bf82b829a63f77a79
smallriver0316 <masahiro@TECHNODROME.localdomain>
2021-02-13 00:22:58 +0900

    Succeeded to push doorkeeper events and compass events of one month

--- src/lambda/compass-collector/handler.js
+++ src/lambda/compass-collector/handler.js
@@ -1,53 +1,72 @@
 'use strict';
 const moment = require('moment');
 var AWS = require('aws-sdk');
-var kinesisClient = new AWS.Kinesis({apiVersion: '2013-12-02'});
+var kinesisClient = new AWS.Kinesis({ apiVersion: '2013-12-02' });
 
 var KinesisStream = require('../../aws/kinesis-stream');
 const callExternalEventApi = require('../request-event');
 
+const MAX_EVENTS_NUM = 2000;
+
 module.exports.compassCollector = async (event, context, callback) => {
   console.log('Start compassCollector handler');
   const currentMonth = moment().format('YYYYMM');
-  const url = `https://connpass.com/api/v1/event/?ym=${currentMonth}`;
+
+  try {
+    const res = await requestCompassData(currentMonth, 1);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(res)
+    });
+  } catch(err) {
+    callback(new Error(err))
+  }
+};
+
+async function requestCompassData(currentMonth, startIndex) {
+  console.log(`Start request to Compass for month: ${currentMonth}, startIndex: ${startIndex}`);
+
+  const url = `https://connpass.com/api/v1/event/?ym=${currentMonth}&start=${startIndex}`;
   const options = {
     headers: {
       'User-Agent': 'Node/12.x'
     }
   };
 
-  try {
-    const compassRes = await callExternalEventApi(url, options);
-    const compassData = JSON.parse(compassRes);
-    const events = compassData.events.map(event => {
-      return {
-        id: 'compass' + event.event_id,
-        origin: 'compass',
-        event_id: event.event_id,
-        title: event.title,
-        description: event.description,
-        event_url: event.event_url,
-        start_time: event.started_at,
-        end_time: event.ended_at,
-        ticket_limit: event.limit,
-        accepted: event.accepted,
-        waiting: event.waiting,
-        updated_at: event.updated_at,
-        place: event.place,
-        address: event.address,
-        lat: event.lat,
-        lon: event.lon
-      };
-    });
+  const compassRes = await callExternalEventApi(url, options);
+  const compassData = JSON.parse(compassRes);
+  const allEvents = compassData.results_available > MAX_EVENTS_NUM ? MAX_EVENTS_NUM : compassData.results_available;
+  const requestedEvents = compassData.results_returned;
+  const remainedEvents = allEvents - (startIndex + requestedEvents - 1);
 
-    const kinesisStream = new KinesisStream(kinesisClient);
-    const kinesisRes = await kinesisStream.putRecords(events);
+  console.log(`All events: ${allEvents}, Fetched events: ${requestedEvents}, Remained events: ${remainedEvents}`);
 
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(kinesisRes)
-    });
-  } catch(err) {
-    callback(new Error(err))
+  const events = compassData.events.map(event => {
+    return {
+      id: 'compass' + event.event_id,
+      origin: 'compass',
+      event_id: event.event_id,
+      title: event.title,
+      description: event.description,
+      event_url: event.event_url,
+      start_time: event.started_at,
+      end_time: event.ended_at,
+      ticket_limit: event.limit,
+      accepted: event.accepted,
+      waiting: event.waiting,
+      updated_at: event.updated_at,
+      place: event.place,
+      address: event.address,
+      lat: event.lat,
+      lon: event.lon
+    };
+  });
+
+  const kinesisStream = new KinesisStream(kinesisClient);
+  const res = await kinesisStream.putRecords(events);
+  if (remainedEvents > 0) {
+    return requestCompassData(currentMonth, startIndex + requestedEvents);
+  } else {
+    return res;
   }
-};
+}
--- src/lambda/doorkeeper-collector/handler.js
+++ src/lambda/doorkeeper-collector/handler.js
@@ -1,9 +1,17 @@
 'use strict';
+const moment = require('moment');
+var AWS = require('aws-sdk');
+var kinesisClient = new AWS.Kinesis({ apiVersion: '2013-12-02' });
+
+var KinesisStream = require('../../aws/kinesis-stream');
 const callExternalEventApi = require('../request-event');
 
 module.exports.doorkeeperCollector = async (event, context, callback) => {
   console.log('Start doorkeeperCollector handler');
-  const url = 'https://api.doorkeeper.jp/events/28319';
+
+  const startOfMonth = moment().startOf('month').format('YYYYMMDD');
+  const endOfMonth = moment().endOf('month').format('YYYYMMDD');
+  const url = `https://api.doorkeeper.jp/events/?page=1&since=${startOfMonth}&until=${endOfMonth}`;
   const options = {
     headers: {
       'User-Agent': 'Node/12.x',
@@ -12,10 +20,34 @@ module.exports.doorkeeperCollector = async (event, context, callback) => {
   };
 
   try {
-    const dkRes = await callExternalEventApi(url, options);
+    const doorkeeperRes = await callExternalEventApi(url, options);
+    const doorkeeperData = JSON.parse(doorkeeperRes);
+    const events = doorkeeperData.map(data => {
+      return {
+        id: 'doorkeeper' + data.event.id,
+        origin: 'doorkeeper',
+        event_id: data.event.id,
+        title: data.event.title,
+        description: data.event.description,
+        event_url: data.event.public_url,
+        start_time: data.event.starts_at,
+        end_time: data.event.ends_at,
+        ticket_limit: data.event.ticket_limit,
+        accepted: data.event.participants,
+        waiting: data.event.waitlisted,
+        updated_at: data.event.updated_at,
+        place: data.event.venue_name,
+        address: data.event.address,
+        lat: data.event.lat,
+        lon: data.event.long
+      };
+    });
+
+    const kinesisStream = new KinesisStream(kinesisClient);
+    const res = await kinesisStream.putRecords(events);
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(dkRes)
+      body: JSON.stringify(res)
     });
   } catch(err) {
     callback(new Error(err));
